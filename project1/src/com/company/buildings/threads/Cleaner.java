a0e02128dfbd09904832a62139c55d5340ff0a2d
my_name <10ilay1998@gmail.com>
2018-11-16 17:36:05 +0400

    add threads

--- project1/src/com/company/buildings/threads/Cleaner.java
+++ project1/src/com/company/buildings/threads/Cleaner.java
@@ -1,5 +1,31 @@
 package com.company.buildings.threads;
 
+import com.company.buildings.Building;
+import com.company.buildings.Floor;
+import com.company.buildings.Space;
+
 public class Cleaner extends Thread{
+
+    Building building;
+
+    public Cleaner(Building building){
+        this.building = building;
+    }
+
+    @Override
+    public void run(){
+        int i = 0;
+        for (Floor floor:
+                this.building
+             ) {
+            for (Space space :
+                    floor
+                 ) {
+                System.out.println("Cleaning space number " +  i++ + " with total area " + space.getSquare() + " square meters");
+            }
+            
+        }
+    }
+    
 
 }
